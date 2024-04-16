5ab4a9190a3e60cb97d3789be60b2495a65f3a47
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2024-04-16 20:21:40 +0200

    Agregado index.html, style.css y mail.js

---
+++ main.js
@@ -0,0 +1,10 @@
+const nextButton = document.getElementById("next-btn");
+const restartButton = document.getElementById("restart-btn");
+const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-buttons");
+const homeVista = document.getElementById('home-vista');
+const takeTheQuizBtn = document.getElementById('take-the-Quiz');
+const noteContainer= document.querySelector('.note-container')
+const punctuation=document.getElementById('note-punctuation')
+
