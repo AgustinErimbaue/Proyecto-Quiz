cd0197d205b83f412de6f07aaf6fb27e5009959e
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2024-04-16 20:48:22 +0200

    Agregada funcionalidad de inicio de juego y carga de preguntas desde la API

--- main.js
+++ main.js
@@ -8,3 +8,23 @@ const takeTheQuizBtn = document.getElementById('take-the-Quiz');
 const noteContainer= document.querySelector('.note-container')
 const punctuation=document.getElementById('note-punctuation')
 
+let questions = [];
+let currentQuestionIndex = 0;
+let note = 0
+
+axios.get("https://quizapi.io/api/v1/questions?apiKey=mbXOtx4KgoWNLReBCDYkMsgsCQKC52TY77512IAf&limit=10")
+    .then((res) => {
+        questions = res.data;
+        console.log(questions);
+       
+    })
+    .catch((err) => console.log(err));
+
+
+// Función de inicio del juego
+const startGame = () => {
+    homeVista.classList.add("hide");
+    questionContainerElement.classList.remove("hide");
+    currentQuestionIndex = 0;
+    setNextQuestion();
+}
